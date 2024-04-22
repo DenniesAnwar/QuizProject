8c79ff3bfcf4a86e2e90947fa28499e06c7bc929
Dennies PC <denniesanwar27@gmail.com>
2024-04-22 14:03:04 -0400

    Finished adding logic to  quiz brain adding next_question and check_answer methods

--- venv/main.py
+++ venv/main.py
@@ -1,11 +1,17 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
 for question in question_data:
-  question_text = question["text"]
-  question_answer = question["answer"]
-  new_question = Question(text = question_text, answer = question_answer)
+  question_text = question["question"]
+  question_answer = question["correct_answer"]
+  new_question = Question(question_text,question_answer)
   question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+  quiz.next_question()
+
+print("Congratulations on finishing the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
